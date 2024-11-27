0053accc0fb45a447d08f32b044e8ba40b083000
DrunkTaric <comborush.init@proton.me>
2024-11-27 10:24:56 +0200

    jobs object

---
+++ configs/sh_jobs.lua
@@ -0,0 +1,64 @@
+Drunk.Jobs = {
+    ["police"] = {
+        name = "police",
+        label = "Los Santos Police Department",
+        ranks = {
+            [1] = {
+                name = "recruit",
+                label = "Recruit",
+                pay = 50
+            },
+            [2] = {
+                name = "officer",
+                label = "Officer",
+                pay = 100
+            },
+            [3] = {
+                name = "sergeant",
+                label = "Sergeant",
+                pay = 150
+            },
+            [4] = {
+                name = "lieutenant",
+                label = "Lieutenant",
+                pay = 200
+            },
+            [5] = {
+                name = "captain",
+                label = "Captain",
+                pay = 250
+            },
+            [6] = {
+                name = "boss",
+                label = "Boss",
+                pay = 300
+            }
+        }
+    },
+    ["ambulance"] = {
+        name = "ambulance",
+        label = "Los Santos Medical Department",
+        ranks = {
+            [1] = {
+                name = "recruit",
+                label = "Recruit",
+                pay = 50
+            },
+            [2] = {
+                name = "doctor",
+                label = "Doctor",
+                pay = 100
+            },
+            [3] = {
+                name = "chief_doctor",
+                label = "Chief Doctor",
+                pay = 150
+            },
+            [4] = {
+                name = "boss",
+                label = "Boss",
+                pay = 200
+            }
+        }
+    }
+}
